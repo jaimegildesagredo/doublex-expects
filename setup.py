8dfb35b095d3310fc5132abe9fa33149f4f4ba63
Jaime Gil de Sagredo Luna <jaimegildesagredo@gmail.com>
2014-06-05 22:41:48 +0200

    Only supports python 2.7

--- setup.py
+++ setup.py
@@ -28,8 +28,6 @@ setup(
         'Operating System :: OS Independent',
         'Programming Language :: Python',
         'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3.3',
-        'Programming Language :: Python :: Implementation :: PyPy',
         'Topic :: Software Development :: Libraries :: Python Modules',
         'License :: OSI Approved :: Apache Software License'
     ]
