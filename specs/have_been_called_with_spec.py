0898960234d5943f51aa7710d5b13bcbb1d09a0c
Jaime Gil de Sagredo Luna <jaimegildesagredo@gmail.com>
2015-01-13 21:08:07 +0100

    Rename examples

--- specs/have_been_called_with_spec.py
+++ specs/have_been_called_with_spec.py
@@ -15,22 +15,22 @@ with describe('have_been_called_with'):
         self.arg2 = 'foobar'
         self.kwargs = {'foo': 1, 'bar': 2}
 
-    with it('should pass if called with positional arg'):
+    with it('passes if called with positional arg'):
         self.method(self.arg1)
 
         expect(self.method).to(have_been_called_with(self.arg1))
 
-    with it('should pass if called with multiple positional args'):
+    with it('passes if called with multiple positional args'):
         self.method(self.arg1, self.arg2)
 
         expect(self.method).to(have_been_called_with(self.arg1, self.arg2))
 
-    with it('should pass if called with keyword args'):
+    with it('passes if called with keyword args'):
         self.method(**self.kwargs)
 
         expect(self.method).to(have_been_called_with(**self.kwargs))
 
-    with it('should pass if called with positional and keyword args'):
+    with it('passes if called with positional and keyword args'):
         self.method(self.arg1, self.arg2, **self.kwargs)
 
         expect(self.method).to(have_been_called_with(self.arg1, self.arg2, **self.kwargs))
@@ -53,43 +53,43 @@ with describe('have_been_called_with'):
         expect(self.method).to(have_been_called_with(
             **{k: be_a(type(v)) for k,v in self.kwargs.items()}))
 
-    with it('should fail if not called with positional arg'):
+    with it('fails if not called with positional arg'):
         self.method()
 
         with failure('to have been called with {!r}'.format(self.arg1)):
             expect(self.method).to(have_been_called_with(self.arg1))
 
-    with it('should fail if not called with keyword args'):
+    with it('fails if not called with keyword args'):
         self.method()
 
         with failure('been called with {}'.format(plain_enumerate((), self.kwargs))):
             expect(self.method).to(have_been_called_with(**self.kwargs))
 
     with context('#negated'):
-        with it('should pass if called with different positional arg'):
+        with it('passes if called with different positional arg'):
             self.method(self.arg1)
 
             expect(self.method).not_to(have_been_called_with(self.arg1, self.arg2))
 
-        with it('should fail if called with args'):
+        with it('fails if called with args'):
             self.method(self.arg1, **self.kwargs)
 
             with failure('not to have been called with {}'.format(plain_enumerate((self.arg1,), self.kwargs))):
                 expect(self.method).not_to(have_been_called_with(self.arg1, **self.kwargs))
 
     with describe('once'):
-        with it('should pass if called with args once'):
+        with it('passes if called with args once'):
             self.method(self.arg1)
 
             expect(self.method).to(have_been_called_with(self.arg1).once)
 
-        with it('should pass if called with args once and with other args'):
+        with it('passes if called with args once and with other args'):
             self.method(self.arg1)
             self.method()
 
             expect(self.method).to(have_been_called_with(self.arg1).once)
 
-        with it('should fail if called with args more than once'):
+        with it('fails if called with args more than once'):
             self.method(self.arg1)
             self.method(self.arg1)
 
